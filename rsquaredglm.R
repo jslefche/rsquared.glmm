ca55c76a86d24beb46a386500e1f31dfce8f24fb
jslefche <jslefche@vims.edu>
2013-11-04 11:23:20 -0500

    Update rsquaredglm.R
    Minor changes to example data (again)

--- rsquaredglm.R
+++ rsquaredglm.R
@@ -7,7 +7,8 @@ rsquared.glme=function(modlist) {
   do.call(rbind,lapply(modlist,function(i) {
     # For models fit using lm
     if(class(i)=="lm") {
-      Rsquared.mat=data.frame(Class=class(i),Family="Gaussian",Marginal=summary(i)$r.squared,
+      Rsquared.mat=data.frame(Class=class(i),Family="Gaussian",
+                              Marginal=summary(i)$r.squared,
                               Conditional=NA,AIC=AIC(i)) }
     # For general linear models fit using lme4
     else if(class(i)=="lmerMod" | class(i)=="merLmerTest") {
@@ -22,8 +23,8 @@ rsquared.glme=function(modlist) {
       # Calculate conditional R-squared (fixed effects+random effects/total variance)
       Rc=(VarF+VarRand)/(VarF+VarRand+VarResid)
       # Bind R^2s into a matrix and return with AIC values
-      Rsquared.mat=data.frame(Class=class(i),Family="Gaussian",Marginal=Rm,Conditional=Rc,
-                              AIC=AIC(update(i,REML=F))) }
+      Rsquared.mat=data.frame(Class=class(i),Family="Gaussian",Marginal=Rm,
+                              Conditional=Rc,AIC=AIC(update(i,REML=F))) }
     #For generalized linear models (family=="binomial") fit using lme4
     else if(class(i)=="glmerMod" & summary(i)$family=="binomial") {
       # Get variance of fixed effects by multiplying coefficients by design matrix
@@ -37,7 +38,8 @@ rsquared.glme=function(modlist) {
       # Calculate conditional R-squared (fixed effects+random effects/total variance)
       Rc=(VarF+VarRand)/(VarF+VarRand+pi^2/3)
       # Bind R^2s into a matrix and return with AIC values
-      Rsquared.mat=data.frame(Class=class(i),Family=summary(i)$family,Marginal=Rm,Conditional=Rc,AIC=AIC(i)) }
+      Rsquared.mat=data.frame(Class=class(i),Family=summary(i)$family,Marginal=Rm,
+                              Conditional=Rc,AIC=AIC(i)) }
     #For generalized linear models (family=="poisson") fit using lme4
     else if(class(i)=="glmerMod" & summary(i)$family=="poisson") {
       # Get variance of fixed effects by multiplying coefficients by design matrix
@@ -50,14 +52,15 @@ rsquared.glme=function(modlist) {
       fixef.names=do.call(rbind,lapply(1:length(names(fixef(i))[-1]),function(j) {
         d=colnames(i@frame)[pmatch(colnames(i@frame),names(fixef(i))[-1][j])>0]
         d[!is.na(d)] } ) )[,1]
-      # Generate null model (intercept and random effects only, removed fixed effects)
+      # Generate null model (intercept and random effects only, no fixed effects)
       null.mod=update(i,paste(".~.-",paste(fixef.names,collapse="-"),sep=""))
       # Calculate marginal R-squared
       Rm=VarF/(VarF+VarRand+log(1+1/exp(as.numeric(fixef(null.mod)))))
       # Calculate conditional R-squared (fixed effects+random effects/total variance)
       Rc=(VarF+VarRand)/(VarF+VarRand+log(1+1/exp(as.numeric(fixef(null.mod)))))
       # Bind R^2s into a matrix and return with AIC values
-      Rsquared.mat=data.frame(Class=class(i),Family=summary(i)$family,Marginal=Rm,Conditional=Rc,AIC=AIC(i)) }
+      Rsquared.mat=data.frame(Class=class(i),Family=summary(i)$family,Marginal=Rm,
+                              Conditional=Rc,AIC=AIC(i)) }
     # For model fit using nlme
     else if(class(i)=="lme") {
       # Get design matrix of fixed effects from model
@@ -65,7 +68,8 @@ rsquared.glme=function(modlist) {
       # Get variance of fixed effects by multiplying coefficients by design matrix
       VarF=var(as.vector(fixef(i) %*% t(Fmat)))
       # Get variance of random effects by extracting variance components
-      VarRand=sum(suppressWarnings(as.numeric(VarCorr(i)[rownames(VarCorr(i))!="Residual",1])),na.rm=T)
+      VarRand=sum(suppressWarnings(as.numeric(VarCorr(i)
+                  [rownames(VarCorr(i))!="Residual",1])),na.rm=T)
       # Get residual variance
       VarResid=as.numeric(VarCorr(i)[rownames(VarCorr(i))=="Residual",1])
       # Calculate marginal R-squared (fixed effects/total variance)
@@ -81,8 +85,10 @@ rsquared.glme=function(modlist) {
 ### Examples ###
 
 # set.seed(9)
-# data=data.frame(y=rnorm(100,5,10),fixed1=rnorm(100,20,100),fixed2=rnorm(100,0.5,2),
-# rand1=LETTERS[1:2],rand2=c(rep("C",50),rep("D",50)))
+# data=data.frame(y=rnorm(100,5,10),y.binom=rbinom(100,1,0.5),
+#  y.poisson=rpois(100,5),fixed1=rnorm(100,20,100),
+#  fixed2=rnorm(100,0.5,2),rand1=LETTERS[1:2],
+#  rand2=c(rep("C",50),rep("D",50)))
 # 
 # library(lme4)
 # #Linear model
@@ -92,12 +98,10 @@ rsquared.glme=function(modlist) {
 # mod2=lmer(y~fixed1+fixed2+(1|rand2/rand1),data)
 # rsquared.glme(list(mod0,mod1,mod2))
 # #Generalized linear mixed effects model (binomial)
-# binom.response=rbinom(100,1,0.5)
-# mod3=glmer(binom.response~fixed1+fixed2+(1|rand2/rand1),family="binomial",data)
+# mod3=glmer(y.binom~fixed1+fixed2+(1|rand2/rand1),family="binomial",data)
 # rsquared.glme(list(mod3))
 # #Generalized linear mixed effects model (poisson)
-# poisson.response=rpois(100,5)
-# mod4=glmer(poisson.response~fixed1+fixed2+(1|rand2/rand1),family="poisson",data)
+# mod4=glmer(y.poisson~fixed1+fixed2+(1|rand2/rand1),family="poisson",data)
 # rsquared.glme(list(mod4))
 # #Get values for all kinds of models
 # lmer.models=rsquared.glme(list(mod0,mod1,mod2,mod3,mod4));lmer.models
@@ -111,7 +115,7 @@ rsquared.glme=function(modlist) {
 # library(lmerTest)
 # lmerTest.models=rsquared.glme(list(mod0,mod1,mod2,mod3,mod4)); lmerTest.models
 # lmer.models==lmerTest.models #This is generating odd results
-
+# 
 # detach(package:lme4,unload=T) #Parts of this package conflict with lme4
 # require(nlme)
 # mod0=lm(y~fixed1,data)
